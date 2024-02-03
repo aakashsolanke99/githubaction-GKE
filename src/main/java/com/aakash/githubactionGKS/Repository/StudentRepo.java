95138fcf98b3bf42822c3a00bb7e48f15a44fabd
aakashsolanke99 <aakashsolanke99@gmail.com>
2024-02-03 16:00:43 +0530

    coonect with gcp sql

---
+++ src/main/java/com/aakash/githubactionGKS/Repository/StudentRepo.java
@@ -0,0 +1,9 @@
+package com.aakash.githubactionGKS.Repository;
+
+import com.aakash.githubactionGKS.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student,Integer> {
+}
--- src/main/java/com/aakash/githubactionGKS/controller/HelloWorldController.java
+++ src/main/java/com/aakash/githubactionGKS/controller/HelloWorldController.java
@@ -1,13 +1,29 @@
 package com.aakash.githubactionGKS.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.aakash.githubactionGKS.Repository.StudentRepo;
+import com.aakash.githubactionGKS.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    public StudentRepo studentRepo;
     @GetMapping("hello/{name}")
     public String sayHello(@PathVariable String name){
         return "Hi "+name+" how are you ";
+    }
+
+    @GetMapping("/getAllstudents")
+    public List<Student> getAllStudent(){
+        return studentRepo.findAll();
+    }
+
+    @PostMapping("/saveStudent")
+    public Student saveStudent(@RequestBody Student student){
+        return studentRepo.save(student);
     }
 }
