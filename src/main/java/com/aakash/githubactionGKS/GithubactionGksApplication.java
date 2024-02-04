8dd61db0c131b2dfd50a2ed9014b4277ff32201c
aakashsolanke99 <aakashsolanke99@gmail.com>
2024-02-04 22:32:32 +0530

    cluster ip added

--- src/main/java/com/aakash/githubactionGKS/GithubactionGksApplication.java
+++ src/main/java/com/aakash/githubactionGKS/GithubactionGksApplication.java
@@ -3,12 +3,16 @@ package com.aakash.githubactionGKS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class GithubactionGksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubactionGksApplication.class, args);
-		System.out.println("hi hhh");
+
 	}
+
 
 }
